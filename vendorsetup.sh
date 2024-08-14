62bdfb97e44513b0280148aa8b156be1f0389497
HarsharajIrkar <86718309+HarsharajIrkar@users.noreply.github.com>
2024-08-14 13:58:44 +0530

    Updated vendorsetup.sh
    added Miui_cam vendor repository.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,4 @@ git clone -b 14 https://github.com/HarsharajIrkar/android_device_xiaomi_sm6150-c
 git clone -b 14 https://github.com/HarsharajIrkar/proprietary_vendor_xiaomi_violet vendor/xiaomi/violet
 git clone -b 14 https://github.com/HarsharajIrkar/proprietary_vendor_xiaomi_sm6150-common vendor/xiaomi/sm6150
 git clone -b 14 https://github.com/HarsharajIrkar/android_kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
+git clone -b 14 https://gitlab.com/kibria5/android_vendor_MiuiCamera vendor/MiuiCamera
