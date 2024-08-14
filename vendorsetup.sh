edb76313aedb60e61c06012ffe8b011cc5deffb6
HarsharajIrkar <86718309+HarsharajIrkar@users.noreply.github.com>
2024-08-14 13:36:04 +0530

    Created vendorsetup.sh
    created vendorsetup.sh and added all the necessary repositories, which are needed to be cloned.

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone -b 14 https://github.com/HarsharajIrkar/android_device_xiaomi_sm6150-common device/xiaomi/sm6150-common
+git clone -b 14 https://github.com/HarsharajIrkar/proprietary_vendor_xiaomi_violet vendor/xiaomi/violet
+git clone -b 14 https://github.com/HarsharajIrkar/proprietary_vendor_xiaomi_sm6150-common vendor/xiaomi/sm6150
+git clone -b 14 https://github.com/HarsharajIrkar/android_kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
