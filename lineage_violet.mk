26f4d8ae24fb803810b869f91f01c4e72ff1853d
HarsharajIrkar <86718309+HarsharajIrkar@users.noreply.github.com>
2024-08-14 14:20:35 +0530

    Updated lineage_violet.mk
    added path to MemeCam's config.mk

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -12,6 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
+
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
