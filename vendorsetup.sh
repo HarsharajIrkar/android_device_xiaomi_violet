cebe81be32b475a56ae289660295efccb179e42e
Harsh <86718309+HarsharajIrkar@users.noreply.github.com>
2024-08-31 13:19:28 +0530

    violet: added hardware/xiaomi to clone

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,4 @@
-git clone -b 14 https://github.com/HarsharajIrkar/android_device_xiaomi_sm6150-common device/xiaomi/sm6150-common
 git clone -b 14 https://github.com/HarsharajIrkar/proprietary_vendor_xiaomi_violet vendor/xiaomi/violet
-git clone -b 14 https://github.com/HarsharajIrkar/proprietary_vendor_xiaomi_sm6150-common vendor/xiaomi/sm6150-common
-git clone -b 14 https://github.com/HarsharajIrkar/android_kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
+git clone -b 14 https://github.com/HarsharajIrkar/android_kernel_xiaomi_sm6150 kernel/xiaomi/violet
 git clone -b 14 https://gitlab.com/kibria5/android_vendor_MiuiCamera vendor/MiuiCamera
+git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
